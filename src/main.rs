d19e52d16592ad2a7d2b449b40f306aaa3c11ecb
Killar.exe <killar.exe@gmail.com>
2024-08-10 23:30:21 +0200

    Switched to Raylib

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-mod window;
 mod chip8;
 mod args;
 
 use clap::Parser;
 
-use std::fs::read;
-use std::time::Duration;
+use std::{fs::read, thread, time};
 
-use sdl2::{pixels::{Color, PixelFormatEnum}, render::{Texture, TextureCreator, TextureAccess}, video::WindowContext};
-use window::*;
+use raylib::prelude::*;
+
 use chip8::*;
 use args::*;
 
 fn main() {
     let args: Args = Args::parse();
-    let mut window: SdlWindow = SdlWindow::new("Test window", 1280, 720, Color::RGB(255, 0, 0));
-    let texture_creator: TextureCreator<WindowContext> = window.get_canvas().texture_creator();
-    let mut screen_texture: Texture = texture_creator.create_texture(Some(PixelFormatEnum::RGBA8888), TextureAccess::Streaming, 64, 32).unwrap();
+    let (mut window, thread) = raylib::init()
+        .size(1280, 720)
+        .title("Chip8 emulator")
+        .build();
     let mut chip8: Chip8 = Chip8::new();
 
     if let Ok(rom) = read(args.rom_file) {
@@ -25,29 +24,25 @@ fn main() {
         }
     }
 
-    while !window.is_window_needs_quit() {
-        chip8.cycle();
-        window.update_event();
-        window.prepare_render();
-
-        let mut screen_buffer: [u8; 64 * 32 * 4] = [0u8; 64 * 32 * 4];
+    let threshold: f32 = 1.0 / 60.0;
+    let mut current_time: f32 = 0.0;
+    let pixel_size: (i32, i32) = (1280 / 64, 720 / 32);
+    while !window.window_should_close() {
+        let delta: f32 = window.get_frame_time();
+        if current_time > threshold {
+            chip8.cycle();
+            current_time = 0.0;
+        } else {
+            current_time += delta;
+        }
+        let mut draw = window.begin_drawing(&thread);
+        draw.clear_background(Color::BLACK);
         for (index, byte) in chip8.pixel_buffer.iter().enumerate(){
+            let x: i32 = (index % 64) as i32;
+            let y: i32 = f32::floor(index as f32 / 64.0) as i32;
             if *byte == 1 {
-                screen_buffer[index * 4] = 0xFF;
-                screen_buffer[index * 4 + 1] = 0xFF;
-                screen_buffer[index * 4 + 2] = 0xFF;
-                screen_buffer[index * 4 + 3] = 0xFF;
-            } else {
-                screen_buffer[index * 4] = 0x00;
-                screen_buffer[index * 4 + 1] = 0x00;
-                screen_buffer[index * 4 + 2] = 0x00;
-                screen_buffer[index * 4 + 3] = 0x00;
+                draw.draw_rectangle(x * pixel_size.0, y * pixel_size.1, pixel_size.0, pixel_size.1, Color::GREEN);
             }
         }
-        screen_texture.update(None, &screen_buffer, 64 * 4).unwrap();
-
-        window.get_canvas_mut().copy(&screen_texture, None, None).unwrap();
-        window.render();
-        std::thread::sleep(Duration::new(0, 16 * 1000 * 1000));
     }
 }
